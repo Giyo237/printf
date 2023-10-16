430cbe472184a959a7e323d848b593b55bd2d06d
kalifa <kalifkalifa@gmail.com>
2023-10-16 16:55:23 -0700

    day 2

---
+++ printf.c
@@ -0,0 +1,58 @@
+#include "printf_heade.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _printf - function that mimics the printf function
+ *
+ * @format: format specifier checker
+ * @...: The variable number of args
+ * Return: count or number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int c = 0;
+	va_list tse;
+	char *str;
+
+	va_start(tse, format);
+	while (*format)
+	{
+		if (*format == '%'){
+			format++;
+		if (*format == '%')
+		{
+			putchar('%');
+			c++;
+		}
+		else if (*format == 'c')
+		{
+			int ch;
+
+		ch = (char) va_arg(tse, int);
+		putchar(ch);
+			c++;
+		}
+		else if (*format == 's')
+		{
+			str = va_arg(tse, char *);
+			while (*str)
+			{
+				putchar(*str);
+				str++;
+				c++;
+			}
+		}
+		}
+		else
+		{
+			putchar(*format);
+			c++;
+		}
+
+		format++;
+	}
+	va_end(tse);
+return (c);
+}
+
